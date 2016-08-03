f2a7341318b25e9ffbe4d0228ba5bf0a8da94eab
Zdravko Katsarov <jaggedex@gmail.com>
2016-08-03 19:21:49 +0300

    dfddd

--- ReceiptsWebMVCApp/ReceiptsWebMVCApp/Controllers/HomeController.cs
+++ ReceiptsWebMVCApp/ReceiptsWebMVCApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ReceiptsWebMVCApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page44446.";
 
             return View();
         }
