6ec78c05a8cb8b554d46d385b8a8b6dcffd48ad5
Zdravko Katsarov <jaggedex@gmail.com>
2016-08-03 19:07:51 +0300

    Change contact() method

--- ReceiptsWebMVCApp/ReceiptsWebMVCApp/Controllers/HomeController.cs
+++ ReceiptsWebMVCApp/ReceiptsWebMVCApp/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace ReceiptsWebMVCApp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page21.";
 
             return View();
         }
